26d1831e8beb0c23965033d49e4484865905e3e5
Ryota <aya.ryo.718@outlook.jp>
2023-07-14 11:28:28 +0900

    sp版用にサイドバーの変更

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
     },
   ];
   return (
-    <div className="col-span-1 h-full pr-4 md:pr-6">
+    <div className="col-span-1 h-full pr-4 md:pr-6 hidden md:block">
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
